75c59916bc7dd4aa578ceb0c5567b408a7ae61d9
srishtisingh-3009 <64847672+srishtisingh-3009@users.noreply.github.com>
2023-10-19 15:24:18 +0530

    Create DAG2.py

---
+++ dags/DAG2.py
@@ -0,0 +1,30 @@
+import os
+from airflow import DAG
+from time import time_ns
+from datetime import datetime
+from airflow.models.connection import Connection
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+def func():
+    print("DAG2 is running", )
+
+with DAG(
+    dag_id="DAG2", schedule="@once", start_date=datetime(2023, 1, 1), is_paused_upon_creation=False, catchup=False
+) as dag:
+
+
+    task_1 = PythonOperator(
+            task_id='Dag2',
+            python_callable=func
+        )
+  
+  trigger_dependent_dag = TriggerDagRunOperator(
+        task_id="trigger_dependent_dag",
+        trigger_dag_id="DAG1",
+        wait_for_completion=True
+    )
+		
+    end_task = DummyOperator(task_id='end_task', dag=dag)
+
+    task_1 >> trigger_dependent_dag >> end_task
