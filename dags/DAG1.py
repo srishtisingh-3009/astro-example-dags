52dcd30e1e17b1997ca5794bb7ebc4f2c7a8a441
srishtisingh-3009 <64847672+srishtisingh-3009@users.noreply.github.com>
2023-10-28 19:54:12 +0530

    Update DAG1.py

--- dags/DAG1.py
+++ dags/DAG1.py
@@ -7,7 +7,7 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 def func():
-    print("DAG1 is running", )
+    print("DAG1 is running")
 	
 with DAG(
     dag_id="DAG1", schedule="*/2 * * * *", start_date=datetime(2023, 1, 1), is_paused_upon_creation=False, catchup=False
